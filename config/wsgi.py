a49ece4ad4cd849f4977ff732272b8db51a0647c
Darhkrich <richphamous09@gmail.com>
2026-04-18 11:55:13 +0000

    wsgi.py restored

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,17 +1,17 @@
+"""
+WSGI config for config project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
+from config.settings_loader import resolve_settings_module
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-# Temporary workaround for Render free tier: run migrations on startup
-try:
-    # Try to access a table that should exist after migrations
-    from django.db import connection
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT 1 FROM security_blockedip LIMIT 1")
-except Exception:
-    # Table missing → run migrations
-    call_command('migrate', interactive=False)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", resolve_settings_module())
 
 application = get_wsgi_application()
